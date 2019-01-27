24575eac060cfef4cf8f00f09dcdfe6f5862bc69
Leslie León <leslie84@nauta.cu>
2019-01-27 23:48:48 +0000

    Updated DNS stuff

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('login/', views.home, name='home'),
-    path('main/', views.main_view, name='main')
+    path('main/', include('main.urls')),
 ]
